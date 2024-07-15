f93452ce2281d3dd3d7e059eadc4255f9ad744b6
BCSchool <thunder3000lite@gmail.com>
2024-07-15 14:06:58 -0700

    New Features
    -Added central game theme: fonts, buttons, labels, etc
    -Added camera smoothing
    -Added window settings: resolutions and window mode

--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -1,10 +1,41 @@
 extends Node
 
+var main_menu_screen = preload("res://scenes/ui/main_menu_screen.tscn")
+var pause_menu_screen = preload("res://scenes/ui/pause_menu_screen.tscn")
+var level_1 = preload("res://scenes/levels/level1.tscn")
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color(0.44, 0.12, 0.53, 1.00))
+	
+	SettingsManager.load_settings()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func start_game():
+	if get_tree().paused:
+		continue_game()
+		return
+	transition_to_scene(level_1.resource_path)
+	
+
+func exit_game():
+	get_tree().quit()
+
+
+func pause_game():
+	get_tree().paused = true
+	
+	var pause_menu_scree_instance = pause_menu_screen.instantiate()
+	get_tree().get_root().add_child(pause_menu_scree_instance)
+
+
+func continue_game():
+	get_tree().paused = false
+
+
+func main_menu():
+	var main_menu_screen_instance = main_menu_screen.instantiate()
+	get_tree().get_root().add_child(main_menu_screen_instance)
+
+func transition_to_scene(scene_path):
+	await get_tree().create_timer(0.1).timeout
+	get_tree().change_scene_to_file(scene_path)
